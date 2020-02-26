02ee050f86ad830176a74d971fed6a522b3af8ae
Austin <austinc.rinker@gmail.com>
2020-02-25 18:33:59 -0800

    Grills move and add pop-up window confirmation

--- giggly-ui/src/Components/EndOfDay.js
+++ giggly-ui/src/Components/EndOfDay.js
@@ -1,8 +1,38 @@
 import React from "react";
 import ReactDOM from 'react-dom';
+import Grills from "./Grills"
 import "./EndOfDay.css"
 
 function EndOfDay(){
+
+  const rawMaterials = [
+    {id: '', name: ''},
+    {id: 'vin', name: 'Vinyl'},
+    {id: 'grd', name: 'Grill Plastic Red'},
+    {id: 'gbu', name: 'Grill Plastic Blue'},
+    {id: 'gbk', name: 'Grill Plastic Black'},
+    {id: 'bob', name: 'Bobbers'},
+    {id: 'prd', name: 'Puppy Feet Red'},
+    {id: 'pbu', name: 'Puppy Feet Blue'},
+    {id: 'pbk', name: 'Puppy Feet Black'},
+    {id: 'w2w', name: 'Walker 2-Wheels'},
+    {id: 'w4w', name: 'Walker 4-heels'},
+    {id: 'grm', name: 'Grommets'},
+    {id: 'vlc', name: 'Velcro'},
+    {id: 'stk', name: 'Grill Stickers'},
+    {id: 'nbs', name: 'Noteboard Sticker'},
+    {id: 'gbx', name: 'Grill Boxes'},
+    {id: 'fcr', name: 'Feet Cores'},
+    {id: 'fsw', name: 'Feet Screws'},
+    {id: 'fbx', name: 'Feet Boxes'},
+    {id: 'shp', name: 'Shipping Envelopes'},
+    {id: 'pbg', name: 'Polybags'}
+  ];
+
+  let materialsList = rawMaterials.length > 0 && rawMaterials.map((item, i) => {
+    return (<option key={i} value={item.id}>{item.name}</option>)
+  }, this)
+
   const grillDesign = [
     {id: 'em', name: ''},
     {id: 'tr', name: 'Truck'},
@@ -51,74 +81,11 @@ function EndOfDay(){
     <div>
     <div class="top">
       <div>
-        <h2 class="end">Grill</h2>
-        <form class="inline-date">
-          <label>Date: </label>
-          <input type="type" name="month" placeholder="MM" maxlength="2" size="2"/>
-          <input type="type" name="day" placeholder="DD" maxlength="2" size="2"/>
-          <input type="type" name="year" placeholder="YYYY" maxlength="4" size="2"/>
-        </form>
-
-        <form class="form-inline">
-          <label for="grill_design">Design: </label>
-          <select id="grill_design">{grillDesignList}</select>
-          <label for="grill_color">Colors: </label>
-          <select id="grill_color">{colorsList}</select>
-        </form>
-
-        <form class="form-inline">
-          <label><b>Process</b></label>
-          <label><b>Completed</b></label>
-          <label><b>Lost</b></label>
-        </form>
-
-        <form class="form-inline">
-          <label for="grill_vac">Belovac: </label>
-          <input id="grill_vac" type="text" name="amount" defualtValue="" maxlength="5" size="8"/>
-          <input id="vac_lost" type="text" name="amount" defualtValue="" maxlength="5" size="8"/>
-        </form>
-
-        <form class="form-inline">
-          <label for="grill_guil">Guillotine: </label>
-          <input id="grill_guil" type="text" name="amount" defualtValue="" maxlength="10" size="8"/>
-          <input id="guil_lost" type="text" name="amount" defualtValue="" maxlength="10" size="8"/>
-        </form>
-
-        <form class="form-inline">
-          <label for="grill_zip">RotoZip: </label>
-          <input id="grill_zip" type="text" name="amount" defualtValue="" maxlength="10" size="8"/>
-          <input id="zip_lost" type="text" name="amount" defualtValue="" maxlength="10" size="8"/>
-        </form>
-
-        <form class="form-inline">
-          <label for="grill_sand">Sanding: </label>
-          <input id="grill_sand" type="text" name="amount" defualtValue="" maxlength="10" size="8"/>
-          <input id="sand_lost" type="text" name="amount" defualtValue="" maxlength="10" size="8"/>
-        </form>
-
-        <form class="form-inline">
-          <label for="grill_assem">Assembly: </label>
-          <input id="grill_assem" type="text" name="amount" defualtValue="" maxlength="10" size="8"/>
-          <input id="assem_lost" type="text" name="amount" defualtValue="" maxlength="10" size="8"/>
-        </form>
-
-        <form class="form-inline">
-          <label for="grill_vin">Vinyl: </label>
-          <input id="grill_vin" type="text" name="amount" defualtValue="" maxlength="10" size="8"/>
-          <input id="vvin_lost" type="text" name="amount" defualtValue="" maxlength="10" size="8"/>
-        </form>
-
-        <form class="form-inline">
-          <label for="grill_pac">Packaging: </label>
-          <input id="grill_pac" type="text" name="amount" defualtValue="" maxlength="10" size="8"/>
-          {/*<label for="pac_lost">Lost: </label>*/}
-          <input id="pac_lost" type="text" name="amount" defualtValue="" maxlength="10" size="8"/>
-        </form>
-
-        <input type="submit" value="Add" />
+        <Grills />
         </div>
         <div>
           hey
+          <output name="display" for="grill_vac vac_lost"></output>
         </div>
       </div>
 
