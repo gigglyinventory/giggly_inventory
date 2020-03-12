cec5a4ffeae2ce68dbcbb60183e3f5f3c39b4ab2
BlekerE <49286890+BlekerE@users.noreply.github.com>
2020-03-12 14:14:13 -0700

    Update admin.js

--- react-backend/routes/admin.js
+++ react-backend/routes/admin.js
@@ -4,7 +4,7 @@ var adminController = require('../controllers/admin');
 const isAuth = require('../middleware/is-auth');
 
 router.get('/get-users', adminController.getUsers);
-router.post('/add-user',  adminController.postAddUser);
+router.post('/add-user',  isAuth, adminController.postAddUser);
 router.post('/add-product', isAuth, adminController.postAddProduct);
 router.post('/delete-user', isAuth, adminController.postDeleteUser);
 router.post('/discontinue-product', isAuth, adminController.postDiscontinueProduct);
