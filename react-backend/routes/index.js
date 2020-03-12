49916c950d329be97d62a0f85b97458c8ec12b96
BlekerE <49286890+BlekerE@users.noreply.github.com>
2020-03-12 14:14:38 -0700

    Update index.js

--- react-backend/routes/index.js
+++ react-backend/routes/index.js
@@ -5,7 +5,10 @@ const isAuth = require('../middleware/is-auth');
 
 /* GET home page. */
 router.get('/', /* isAuth, */ function(req, res, next) { // add isAuth after login page is moved?
-  res.render('index', { title: 'Express' });
+  res.render('../../giggly-ui/src/Home', {title: 'gigglyinventory home'}, function (err, html){
+      console.log(html);
+      res.send('done');
+  })
 });
 
 module.exports = router;
